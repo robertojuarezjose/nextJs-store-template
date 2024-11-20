d7894107e257e485ca3ad772c6f73a527bcf3f83
robertojuarezjose <134658833+robertojuarezjose@users.noreply.github.com>
2024-11-19 21:43:20 -0600

    fixing about and admin page

--- app/about/page.tsx
+++ app/about/page.tsx
@@ -7,6 +7,7 @@ function AboutPage() {
 
     return (
         <section>
+
             <h1 className='flex flex-wrap gap-2 sm:gap-x-6 items-center justify-center text-4xl font-bold leading-none tracking-wide sm:text-6xl'>
                 We love
                 <span className='bg-primary py-2 px-4 rounded-lg tracking-widest text-white'>
--- app/admin/Sidebar.tsx
+++ app/admin/Sidebar.tsx
@@ -18,6 +18,7 @@ function Sidebar() {
                         asChild
                         className='w-full mb-2 capitalize font-normal justify-start'
                         variant={variant}
+
                     >
                         <Link key={link.href} href={link.href}>
                             {link.label}
